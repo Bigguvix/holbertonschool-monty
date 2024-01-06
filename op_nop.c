5c3c8c9d85db6e18fed3de3ec7d8964fa02bf57c
Bigguvix <nahufeola@hotmail.com>
2024-01-06 09:45:59 -0800

    nop opcode

---
+++ op_nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * nop - Does nothing lol.
+ *
+ * @st: The head of the stack.
+ * @newl: The number of the line.
+ *
+ * Return: Nothgin.
+ */
+void nop(stack_t **st, unsigned int newl)
+{
+	(void)st;
+	(void)newl;
+}
